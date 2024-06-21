1ae75a3d96a9ee0037af10c04a43b279c444ef62
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-06-21 21:32:06 +0300

    repository annotation added

--- src/main/java/com/example/AdminUserThings/services/UserRepository.java
+++ src/main/java/com/example/AdminUserThings/services/UserRepository.java
@@ -1,11 +1,10 @@
 package com.example.AdminUserThings.services;
 
-
 import com.example.AdminUserThings.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface UserRepository extends CrudRepository<User,Integer> {
-    public User findByEmail(String email);
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByEmail(String email);
 }
