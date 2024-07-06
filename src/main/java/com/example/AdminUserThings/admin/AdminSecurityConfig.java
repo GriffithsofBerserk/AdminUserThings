63d284fb6871477489551098ca771b6ff97a2d6b
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-07-06 20:26:33 +0300

    auth provider for user added

--- src/main/java/com/example/AdminUserThings/admin/AdminSecurityConfig.java
+++ src/main/java/com/example/AdminUserThings/admin/AdminSecurityConfig.java
@@ -3,8 +3,9 @@ package com.example.AdminUserThings.admin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +22,15 @@ public class AdminSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder1(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider1(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsPasswordService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder1());
+
+        return  provider;
     }
     @Bean
     public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception{
